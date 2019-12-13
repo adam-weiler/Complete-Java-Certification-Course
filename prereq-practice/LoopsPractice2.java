8834179cc8565954da32bc5adfbb2741a271251b
adam-weiler <adam.weiler@yahoo.ca>
2019-12-13 13:46:38 -0500

    Even more loops.

--- prereq-practice/LoopsPractice2.java
+++ prereq-practice/LoopsPractice2.java
@@ -13,6 +13,16 @@ public class LoopsPractice2 {
 		for (int i = 0; i <= 100; i+=2) {
 			System.out.println("i: " + i);
 		}
+		
+		for (int idx = 0; idx < 100; idx++) { // Too many nested loops. No good!
+			for (int j = 0; j < 10; j+=2) {
+				for (int k = 0; k < 10; k+=2) {
+					for (int l = 0; l < 10; l+=2) {
+						System.out.println("The value of idx was " + idx + " --- j was " + j + " --- k was " + k + " --- l was " + l);
+					}
+				}
+			}
+		}
 	}	
 	
 }
