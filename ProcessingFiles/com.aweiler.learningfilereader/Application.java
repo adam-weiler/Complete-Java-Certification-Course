d91ae71eb74c69471d99ca81e4c946b0ef3c0ad2
adam-weiler <adam.weiler@yahoo.ca>
2019-12-17 12:19:04 -0500

    Adding better Java 7 version.

--- ProcessingFiles/com.aweiler.learningfilereader/Application.java
+++ ProcessingFiles/com.aweiler.learningfilereader/Application.java
@@ -1,7 +1,5 @@
 package com.aweiler.learningfilereader;
 
-//import java.io.File;
-//import java.io.FileReader;
 import java.io.*;  // Imports everything from java.io
 
 public class Application {
@@ -9,12 +7,9 @@ public class Application {
 	public static void main(String[] args) {
 		
 		File file = new File("myfile.txt");
-		BufferedReader bufferedReader = null;
-		FileReader fileReader = null;
 		
-		try {
-			fileReader = new FileReader(file);
-			bufferedReader = new BufferedReader(fileReader);
+		try (FileReader fileReader = new FileReader(file);
+			 BufferedReader bufferedReader = new BufferedReader(fileReader);) {
 			
 			String line = bufferedReader.readLine();
 			
@@ -27,20 +22,6 @@ public class Application {
 			System.out.println("File not found.");
 		} catch (IOException e) {
 			System.out.println("Problem reading the file " + file.getName());
-		} finally {
-			try {
-				if(bufferedReader != null) {  // If bufferedReader exists, close it.
-					bufferedReader.close();
-				}
-				if(fileReader != null) {  // If fileReader exists, close it.
-					fileReader.close();
-				}
-				
-			} catch (IOException e) {
-				System.out.println("Unable to close file " + file.getName());
-			} // catch(NullPointerException ex) {  // You typically shouldn't do this.
-//				System.out.println(" file was probably never opened " + ex);
-//			}
 		}
 	}
 
